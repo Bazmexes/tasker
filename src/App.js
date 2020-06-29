f14472c6aaa985172f70cc41df2924e6832d382a
Bazarov Maxim <bazarow.max@gmail.com>
2020-06-29 17:27:34 +0800

    after eject

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ class App extends React.Component {
         <Helmet>
           <meta charset="utf-8"/>
           <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
+
         </Helmet>
         <TaskModal/>
         <Layout>
